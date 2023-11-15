d6b168fc1460837d8dc7c87ece65203fd388471b
JBNRZ <3537659915@qq.com>
2023-11-15 12:49:34 +0800

    Fixed bug

--- nonebot2/plugins/nonebot_plugin_chatgpt_turbo/config.py
+++ nonebot2/plugins/nonebot_plugin_chatgpt_turbo/config.py
@@ -1,12 +1,11 @@
 from pydantic import Extra, BaseModel
-from typing import Optional
 
 
 class Config(BaseModel, extra=Extra.ignore):
-    openai_api_keys: Optional[list[str]] = ""
-    openai_model_name: Optional[str] = "gpt-3.5-turbo"
-    openai_max_history_limit: Optional[int] = 5
-    openai_http_proxy: Optional[str] = None
+    openai_api_keys = ""
+    openai_model_name = "gpt-3.5-turbo"
+    openai_max_history_limit = 5
+    openai_http_proxy = None
     enable_private_chat: bool = True
     chatgpt_turbo_public: bool = False  # 群聊是否开启公共会话
 
